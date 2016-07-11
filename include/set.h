45fee91a7aa0d33f66086ae437c3158e7a5dd05e
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-11 22:47:29 +0400

    disjoint-set added

---
+++ include/set.h
@@ -0,0 +1,21 @@
+/**
+	Set implementation (BST)
+	Rasul Kerimov (CoderINusE) 
+*/
+#ifndef __SET_BST__
+#define __SET_BST__
+
+#include <stdlib.h>
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+namespace alg {
+	template <typename T>
+	class Set {
+		
+	};
+} 
+
+#endif
