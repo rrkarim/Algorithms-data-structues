ccf3a51abf87348e47a2934376453d1eb94a9b7f
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-21 22:24:00 +0400

    lru-cache

--- examples/lru-cache.cpp
+++ examples/lru-cache.cpp
@@ -5,21 +5,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 class node {
+public:
   int value;
   int key;
   node *next;
   node *pre;
-public:
   node() {}
-  node(int value, int key) : value(value), key(key), next(NULL), pre(NULL) {}
+  node(int key, int value) : value(value), key(key), next(NULL), pre(NULL) {}
 };
 
 class LRUCache {
-
   int capacity;
   unordered_map <int, node*> map_elems;
   node *head;
@@ -28,9 +27,10 @@ public:
   LRUCache(int capacity) : capacity(capacity) {}
   int get(int key) {
     if(map_elems[key]) {
-      node * curr = make_pair[key];
+      node * curr = map_elems[key];
+      node * newnode = new node(key, curr->value);
       int ret = curr->value;
-      add_to_tail(curr);
+      add_to_tail(newnode);
       remove(curr);
       return ret;
     }
@@ -38,16 +38,58 @@ public:
   }
 
   void add_to_tail(node * n) {
-    
+    if(tail == NULL) {
+      head = tail = n;
+    }
+    else {
+      tail->next = n;
+      tail = tail->next;  
+    }
   }
 
-  int set() {
+  void remove(node *n) {
+    if(n->pre != NULL) {
+      n->pre->next = n->next;
+    }
+    else head = n->next;
 
+    if(n->next != NULL) {
+      n->next->pre = n->pre;
+    }
+    else tail = n->pre;
+    delete n;
+
+  }
+
+  int set(int key, int value) {
+    if(map_elems[key]) {
+      node * curr = map_elems[key];
+      node * newnode = new node(key, value); 
+      add_to_tail(newnode);
+      remove(curr);
+    }
+    else {
+      node * newnode = new node(key, value); 
+      if(map_elems.size() > capacity) {
+        map_elems.erase(head->key);
+        remove(head);
+        add_to_tail(newnode);
+      }
+      else {
+        add_to_tail(newnode);
+      }
+      map_elems[key] = newnode;
+    }
   }
 
 };
 
 int main() {
+  LRUCache * cache = new LRUCache(3);
+  cache->set(1, 34);
+  cache->set(2, 35);
+  cache->set(3, 32);
+  cache->set(4, 31);
 
-
+  cout << cache->get(1) << endl;
 }
